7a270448127c750a09d8f519a9e42eaa7c14396c
hsky <xezw211@126.com>
2016-05-12 22:32:43 +0800

    添加登录页面

---
+++ app/src/main/java/me/hsky/androidshop/userlogin.java
@@ -0,0 +1,18 @@
+package me.hsky.androidshop;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class userlogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_userlogin);
+    }
+
+}
